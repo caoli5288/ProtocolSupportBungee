f920406b685e705492e46eefa27185d5eef4ecf7
MrPowerGamerBR <leonardomalaman@gmail.com>
2018-02-13 22:25:31 +0800

    Fix MOTD for 1.5.X and 1.4.X clients, fixes #45
    (cherry picked from commit b0540ef)

--- src/protocolsupport/protocol/packet/middleimpl/readable/handshake/v_4_5/PingHandshakePacket.java
+++ src/protocolsupport/protocol/packet/middleimpl/readable/handshake/v_4_5/PingHandshakePacket.java
@@ -1,7 +1,4 @@
-package protocolsupport.protocol.packet.middleimpl.readable.handshake.v_4_5_6;
-
-import java.util.Arrays;
-import java.util.Collection;
+package protocolsupport.protocol.packet.middleimpl.readable.handshake.v_4_5;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
@@ -10,7 +7,9 @@ import net.md_5.bungee.protocol.packet.Handshake;
 import net.md_5.bungee.protocol.packet.StatusRequest;
 import protocolsupport.api.ProtocolVersion;
 import protocolsupport.protocol.packet.middleimpl.readable.LegacyDefinedReadableMiddlePacket;
-import protocolsupport.protocol.serializer.StringSerializer;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 public class PingHandshakePacket extends LegacyDefinedReadableMiddlePacket {
 
@@ -20,25 +19,17 @@ public class PingHandshakePacket extends LegacyDefinedReadableMiddlePacket {
 		super(PACKET_ID);
 	}
 
-	protected String host;
-	protected int port;
-
 	@Override
 	protected void read0(ByteBuf from) {
+		System.out.println("Reading 1.5/1.4 ping!");
 		from.readUnsignedByte();
-		from.readUnsignedByte();
-		StringSerializer.readShortUTF16BEString(from);
-		from.readUnsignedShort();
-		from.readUnsignedByte();
-		host = StringSerializer.readShortUTF16BEString(from);
-		port = from.readInt();
 	}
 
 	@Override
 	public Collection<PacketWrapper> toNative() {
 		return Arrays.asList(
-			new PacketWrapper(new Handshake(ProtocolVersion.MINECRAFT_1_7_10.getId(), host, port, 1), Unpooled.EMPTY_BUFFER),
-			new PacketWrapper(new StatusRequest(), Unpooled.wrappedBuffer(readbytes))
+				new PacketWrapper(new Handshake(ProtocolVersion.MINECRAFT_1_7_10.getId(), "127.0.0.1", 25565, 1), Unpooled.EMPTY_BUFFER),
+				new PacketWrapper(new StatusRequest(), Unpooled.wrappedBuffer(readbytes))
 		);
 	}
 
