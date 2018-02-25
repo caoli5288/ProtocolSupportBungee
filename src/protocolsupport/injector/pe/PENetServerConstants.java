f1d9814fb8e9ad2cf21a6b8504d72fef000ac06c
caoli5288 <caoli5288@gmail.com>
2018-02-25 14:06:06 +0800

    Add custom PE welcome message

--- src/protocolsupport/injector/pe/PENetServerConstants.java
+++ src/protocolsupport/injector/pe/PENetServerConstants.java
@@ -1,10 +1,15 @@
 package protocolsupport.injector.pe;
 
+import com.google.common.base.Preconditions;
 import io.netty.channel.Channel;
 import protocolsupport.api.ProtocolVersion;
 import raknetserver.pipeline.raknet.RakNetPacketConnectionEstablishHandler.PingHandler;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 public class PENetServerConstants {
+
+	private static final AtomicReference<String> REFERENCE = new AtomicReference<>("i5mc");
 
 	public static final PingHandler PING_HANDLER = new PingHandler() {
 		@Override
@@ -12,7 +17,7 @@ public class PENetServerConstants {
 			//TODO: fake pspe packets for ping passthrough
 			return String.join(";",
 				"MCPE",
-				"ProtocolSupportBungeePE",
+				REFERENCE.get(),
 				String.valueOf(ProtocolVersion.MINECRAFT_PE.getId()), POCKET_VERSION,
 				"0", "1"
 			);
@@ -22,6 +27,11 @@ public class PENetServerConstants {
 			runnable.run();
 		}
 	};
+
+	public static void setWelcomeMessage(String welcome) {
+		Preconditions.checkNotNull(welcome);
+		REFERENCE.set(welcome);
+	}
 
 	public static final int USER_PACKET_ID = 0xFE;
 	public static final String POCKET_VERSION = "1.2.7";
