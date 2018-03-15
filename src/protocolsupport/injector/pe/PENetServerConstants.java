eb33977da26fbcc032a513eb3e872af550bd8d79
caoli5288 <caoli5288@gmail.com>
2018-03-15 10:31:22 +0800

    Impl pocket server info event
    (cherry picked from commit 03630e12cd41aade873385a08cdc124a16162ca0)

--- src/protocolsupport/injector/pe/PENetServerConstants.java
+++ src/protocolsupport/injector/pe/PENetServerConstants.java
@@ -1,27 +1,21 @@
 package protocolsupport.injector.pe;
 
-import com.google.common.base.Preconditions;
 import io.netty.channel.Channel;
-import net.md_5.bungee.BungeeCord;
 import protocolsupport.api.ProtocolVersion;
+import protocolsupport.api.events.PocketServerInfoEvent;
 import raknetserver.pipeline.raknet.RakNetPacketConnectionEstablishHandler.PingHandler;
 
-import java.util.concurrent.atomic.AtomicReference;
-
 public class PENetServerConstants {
-
-	private static final AtomicReference<String> REFERENCE = new AtomicReference<>("i5mc");
 
 	public static final PingHandler PING_HANDLER = new PingHandler() {
 		@Override
 		public String getServerInfo(Channel channel) {
-			BungeeCord server = BungeeCord.getInstance();
-			int online = server.getOnlineCount();
-			return String.join(";",
-					"MCPE",
-					REFERENCE.get(),
-					String.valueOf(ProtocolVersion.MINECRAFT_PE.getId()), POCKET_VERSION,
-					online + "", server.config.getPlayerLimit() + ""
+            PocketServerInfoEvent event = PocketServerInfoEvent.call("i5mc");
+            return String.join(";",
+				"MCPE",
+                event.getWelcomeMessage(),
+				String.valueOf(ProtocolVersion.MINECRAFT_PE.getId()), POCKET_VERSION,
+				String.valueOf(event.getOnline()), String.valueOf(event.getServerSize())
 			);
 		}
 		@Override
@@ -29,11 +23,6 @@ public class PENetServerConstants {
 			runnable.run();
 		}
 	};
-
-	public static void setWelcomeMessage(String welcome) {
-		Preconditions.checkNotNull(welcome);
-		REFERENCE.set(welcome);
-	}
 
 	public static final int USER_PACKET_ID = 0xFE;
 	public static final String POCKET_VERSION = "1.2.7";
