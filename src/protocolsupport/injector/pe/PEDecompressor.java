9c7164bef7cc846f34ea2b389220e567b1383629
caoli5288 <caoli5288@gmail.com>
2018-08-25 16:23:04 +0800

    Improve PEDecompressor.java

--- src/protocolsupport/injector/pe/PEDecompressor.java
+++ src/protocolsupport/injector/pe/PEDecompressor.java
@@ -1,49 +1,59 @@
 package protocolsupport.injector.pe;
 
 import io.netty.buffer.ByteBuf;
-import io.netty.buffer.ByteBufAllocator;
-import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.codec.DecoderException;
 import io.netty.handler.codec.MessageToMessageDecoder;
 import protocolsupport.protocol.serializer.MiscSerializer;
 import protocolsupport.protocol.serializer.VarNumberSerializer;
 
-import java.text.MessageFormat;
 import java.util.List;
 import java.util.zip.Inflater;
 
 public class PEDecompressor extends MessageToMessageDecoder<ByteBuf> {
 
-    private Inflater inflater;
+    private byte[] buf = new byte[1 << 16];// 64k
+    private Inflater decompress;
 
     @Override
     public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
-        inflater = new Inflater();
+        decompress = new Inflater();
     }
 
     @Override
     public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
-        inflater.end();
+        decompress.end();
     }
 
     @Override
     protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> output) throws Exception {
-        ByteBuf decompression = ByteBufAllocator.DEFAULT.heapBuffer();
+        ByteBuf decompression = ctx.alloc().buffer();
+        _input(in);
         try {
-            inflater.reset();
-            inflater.setInput(MiscSerializer.readAllBytes(in));
-            byte[] buf = new byte[inflater.getRemaining() << 1];
-            for (int ret; (ret = inflater.inflate(buf)) >= 1;) {
-                decompression.writeBytes(buf, 0, ret);
-            }
-            while (decompression.isReadable()) {
-                int length = VarNumberSerializer.readVarInt(decompression);
-                output.add(Unpooled.wrappedBuffer(MiscSerializer.readBytes(decompression, length)));
-            }
+            do {
+                int len = decompress.inflate(buf);
+                decompression.writeBytes(buf, 0, len);
+            } while (!decompress.finished());
+            _output(decompression, output);
         } finally {
+            decompress.reset();
             decompression.release();
         }
+    }
+
+    private void _output(ByteBuf buf, List<Object> output) {
+        do {
+            int len = VarNumberSerializer.readVarInt(buf);
+            output.add(buf.retainedSlice(buf.readerIndex(), len));
+            buf.skipBytes(len);
+        } while (buf.isReadable());
+    }
+
+    private void _input(ByteBuf in) {
+        if (in.hasArray()) {
+            decompress.setInput(in.array(), in.arrayOffset() + in.readerIndex(), in.readableBytes());
+            return;
+        }
+        decompress.setInput(MiscSerializer.readAllBytes(in));
     }
 
 }
