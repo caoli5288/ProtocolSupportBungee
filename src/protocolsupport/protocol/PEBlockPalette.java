349d137fd7b6daa0dd92eb345e5823544fb4f27c
caoli5288 <caoli5288@gmail.com>
2018-11-13 14:20:51 +0800

    Simply PEBlockPalette.java

--- src/protocolsupport/protocol/PEBlockPalette.java
+++ src/protocolsupport/protocol/PEBlockPalette.java
@@ -12,41 +12,27 @@ import protocolsupport.protocol.serializer.StringSerializer;
 import protocolsupport.protocol.serializer.VarNumberSerializer;
 
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class PEBlockPalette {
 
-    private static Map<String, BlockElement> table = new HashMap<>();
     @Getter
     private static byte[] paletteData;
 
     static {
         List<JSONObject> root = (List<JSONObject>) JSONValue.parse(new InputStreamReader(ProtocolSupport.get().getResourceAsStream("block_state_list_1_5.json")));
-
-        List<BlockElement> l = new ArrayList<>(root.size());
-        for (JSONObject obj : root) {
-            l.add(new BlockElement(obj));
-        }
-
         ByteBuf buf = Unpooled.buffer();
         try {
-            VarNumberSerializer.writeVarInt(buf, l.size());
-            for (BlockElement element : l) {
+            VarNumberSerializer.writeVarInt(buf, root.size());
+            for (JSONObject obj : root) {
+                BlockElement element = new BlockElement(obj);
                 StringSerializer.writeVarIntUTF8String(buf, element.name);
                 buf.writeShortLE(element.data);
-                table.put(element.toString(), element);
             }
             paletteData = MiscSerializer.readAllBytes(buf);
         } finally {
             buf.release();
         }
-    }
-
-    public static BlockElement getBlockElement(String nameWithMetadata) {
-        return table.get(nameWithMetadata);
     }
 
     @Data
