38ee1ae362963d578bb7ac9fe34e800fa421a855
caoli5288 <caoli5288@gmail.com>
2018-12-12 10:49:02 +0800

    Fix player list cache when post connector active

--- src/protocolsupport/protocol/packet/middleimpl/readable/play/v_pe_14_15/PlayerListItemPacket.java
+++ src/protocolsupport/protocol/packet/middleimpl/readable/play/v_pe_14_15/PlayerListItemPacket.java
@@ -2,6 +2,8 @@ package protocolsupport.protocol.packet.middleimpl.readable.play.v_pe_14_15;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
+import net.md_5.bungee.UserConnection;
+import net.md_5.bungee.api.event.ServerPostConnectedEvent;
 import net.md_5.bungee.protocol.PacketWrapper;
 import net.md_5.bungee.protocol.packet.PlayerListItem;
 import protocolsupport.api.ProtocolVersion;
@@ -82,6 +84,14 @@ public class PlayerListItemPacket extends PEDefinedReadableMiddlePacket {
             i.setUuid(el);
             return i;
         }).toArray(PlayerListItem.Item[]::new));
+        ServerPostConnectedEvent connector = cache.getPostConnector();
+        if (connector != null) {
+            /*
+             * We handle player list packets manually when post connector active.
+             */
+            ((UserConnection) connection.getPlayer()).getTabListHandler().onUpdate(item);
+            return Collections.emptyList();
+        }
         return Collections.singletonList(new PacketWrapper(item, Unpooled.wrappedBuffer(readbytes)));
     }
 
